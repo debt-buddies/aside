594733161292afaa45509fcb372f7148716f3f7f
KevinFalah <kevin.ghossan@mandalafinance.com>
2025-01-21 22:25:29 +0700

    finishing aside

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import "./App.scss";
+import useStore from "HomeApp/hooks/useStore";
+import { useStoreSelector } from "HomeApp/hooks/useStoreSelector";
 
 const Header = lazy(() => import("HomeApp/Header"));
 
 function App() {
+  const [search, setSearch] = useState("");
+  const listTodos = useStoreSelector((state) => state.todos.listTodos);
+  const { getDataDetail, changePreview } = useStore();
+
+  const lists = listTodos?.filter((ls) => ls.includes(search));
+
+  const handleClick = (detail) => {
+    getDataDetail(detail);
+    changePreview("DETAIL");
+  };
   return (
     <div className="aside">
       <Suspense fallback={<div>loading header...</div>}>
-        <Header subtitle="for aside"  bgColor="red" />
+        <Header title="Daftar list anda" bgColor="white" />
       </Suspense>
-      <div className="mainContent">Ini Aside</div>
+      <hr />
+      <div className="mainContent">
+        {listTodos?.length === 0 ? (
+          <div>Belum ada list</div>
+        ) : (
+          lists.map((ls, i) => (
+            <button key={i} className="item" onClick={() => handleClick(ls)}>
+              {i + 1}. {ls}
+            </button>
+          ))
+        )}
+      </div>
     </div>
   );
 }
